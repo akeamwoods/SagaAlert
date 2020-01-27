3434d8d2b192c1694df9a1f7872c1d5fc30b9320
Akeam Woods <akeam.woods@renishaw.com>
2020-01-27 12:12:30 +0000

    form working almost

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import { render } from "react-dom";
 import { Provider, useDispatch } from "react-redux";
-import { store, useTypedSelector } from "./store";
+import { store } from "./store";
 import { AlertPosition } from "./store/index";
 import { actions } from "./store/actions";
 import { NotificationContainer } from "./components/alert/container";
@@ -11,44 +11,87 @@ import { Form, FormWrapper } from "./style";
 
 function App() {
   const dispatch = useDispatch();
-  const alertInfo = useTypedSelector(state => {
-    return {
-      id: uuid.v4(),
-      title: state.title,
-      message: state.message,
-      duration: state.duration,
-      dismissible: state.dismissible,
-      position: state.position
-    };
-  });
+  const positions = Object.keys(AlertPosition).filter(
+    value => !isNaN(Number(value))
+  );
+
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [durationAllowed, setDurationAllowed] = useState(true);
+  const [duration, setDuration] = useState(0);
+  const [dismissible, setDismissible] = useState(true);
+  const [position, setIndex] = useState(0);
+
+  useEffect(() => {
+    setTitle("");
+    setMessage("");
+    setDurationAllowed(true);
+    setDuration(5000);
+    setDismissible(true);
+    setIndex(0);
+  }, []);
+
+  const onSubmit = () => {
+    if (durationAllowed || dismissible) {
+      dispatch(
+        actions.alertCreated({
+          id: uuid.v4(),
+          title,
+          message,
+          duration,
+          dismissible,
+          position
+        })
+      );
+    } else {
+      // validation here (need either a set duration or dissmissible);
+    }
+  };
+
   return (
     <div className="App">
       <NotificationContainer />
       <FormWrapper>
         <Form>
           <label>Title</label>
-          <input></input>
+          <input value={title} onChange={e => setTitle(e.target.value)}></input>
           <label>Message</label>
-          <input></input>
+          <input
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          ></input>
           <label>Duration?</label>
-          <input type="checkbox"></input>
+          <input
+            checked={durationAllowed}
+            onChange={() => setDurationAllowed(!durationAllowed)}
+            type="checkbox"
+          ></input>
           <label>Duration Length</label>
-          <input type="number"></input>
+          <input
+            value={duration}
+            onChange={e => setDuration(Number(e.target.value))}
+            type="number"
+          ></input>
           <label>Dismissible?</label>
-          <input type="checkbox"></input>
+          <input
+            checked={dismissible}
+            onChange={() => setDismissible(!dismissible)}
+            type="checkbox"
+          ></input>
           <label>Position</label>
-          <select>
-            {Object.keys(AlertPosition)
-              .filter(value => !isNaN(Number(value)))
-              .map(key => (
-                <option value={key}>{AlertPosition[key as any]}</option>
-              ))}
+          <select
+            value={positions[position]}
+            onChange={e => setIndex(e.target.selectedIndex)}
+          >
+            {positions.map(key => (
+              <option value={key}>{AlertPosition[key as any]}</option>
+            ))}
           </select>
           <label></label>
+          <button type="button" onClick={onSubmit}>
+            Create
+          </button>
         </Form>
-        <button onClick={() => dispatch(actions.alertCreated(alertInfo))}>
-          Create
-        </button>
       </FormWrapper>
     </div>
   );
