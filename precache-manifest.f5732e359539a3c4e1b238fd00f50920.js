bd67d7bf330686959d20c067dfd8905af8accd6d
akeamwoods <a19941@hotmail.co.uk>
2020-01-27 21:54:26 +0000

    Updates

---
+++ precache-manifest.f5732e359539a3c4e1b238fd00f50920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2248007a4b83c832baa4690c4526afda",
+    "url": "/SagaAlert/index.html"
+  },
+  {
+    "revision": "5e5119031a8b7376ba5c",
+    "url": "/SagaAlert/static/css/main.d81bedd6.chunk.css"
+  },
+  {
+    "revision": "eea3375546204f07c243",
+    "url": "/SagaAlert/static/js/2.be32437c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/SagaAlert/static/js/2.be32437c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e5119031a8b7376ba5c",
+    "url": "/SagaAlert/static/js/main.82d90e93.chunk.js"
+  },
+  {
+    "revision": "c4973e5d1133926ef480",
+    "url": "/SagaAlert/static/js/runtime-main.ade5b841.js"
+  }
+]);
